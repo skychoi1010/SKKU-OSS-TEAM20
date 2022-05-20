8fded392517ca5df5f809e46f794285cc69ddd98
Sera Choi <100757275+sera2002@users.noreply.github.com>
2022-05-20 18:28:39 +0900

    id 입력란, 식당 카페 선택 추가

--- results.js
+++ results.js
@@ -1,11 +1,26 @@
+const place_type = {
+    restaurent: 0,
+    cafe: 1
+};
+
 function showResults() {
     onDisplay();
+    var start = document.getElementById('starting-point').value;
+    var dest = document.getElementById('destination').value;
+    var user_id = document.getElementById('spotify-id').value;
+
+    var place_radio = document.getElementsByName('place');
+    var place_name = place_radio[0].value;
+    if (place_radio[1].checked == true) {
+        place_name = place_radio[1].value;
+    }
+    
 }
 
 function reset() {
     offDisplay();
-    $('#starting-point').reset();
-    $('#destination').reset();
+    $('#starting-point').reset;
+    $('#destination').reset;
 }
 
 function onDisplay() {
